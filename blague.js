1e7b4c61f4af3ebef4fcc6512607fdc3d98e6576
BIKOUTA Guyverna Mon-Mot Désir <bikoutaguyverna@gmail.com>
2026-08-09 22:17:47 +0100

    feat: implementation de l'appel d'api à official joke api

---
+++ blague.js
@@ -0,0 +1,46 @@
+// Le pont JS -> page. À recopier tel quel dans chaque projet S7 à S9.
+const sortie = document.querySelector("#sortie");
+function afficher(html) {
+  //remplacement au lieu d'incrementer
+  sortie.innerHTML = html;
+}
+
+const url = "https://official-joke-api.appspot.com/random_joke";
+
+const loadingUI = `<article>
+                    <h2 class="loader">Chargment...</h2>
+                </article>`;
+
+const loadJoke = async (url) => {
+  afficher(loadingUI);
+  let errorMessage = "";
+
+  try {
+    const response = await fetch(url);
+
+    if (!response.ok) {
+      throw `${response.status}`;
+    }
+
+    const data = await response.json();
+    if (!data.setup.includes("?")) {
+      loadJoke();
+    }
+    const jokeCard = `<article>
+                    <h3 class="joke-type">${data.type}</h3>
+                    <h2 class="setup">${data.setup}</h2>
+                    <p class="push-line-spoiler">${data.punchline}</p>
+                    <a href="index.html">Une autre</a>
+                </article>`;
+
+    afficher(jokeCard);
+  } catch (error) {
+    errorMessage = `<div class="error">
+                    <p>Error: ${error}</p>
+                    <a class="btn" href="index.html">reessayez</a>
+                </div>`;
+    afficher(errorMessage);
+  }
+};
+
+loadJoke(url);
